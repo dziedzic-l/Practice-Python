214321dfb7717bfeecd9aa61ccfe6b13c63b75e8
Łukasz Dziedzic <pymator@gmail.com>
2019-12-10 09:02:14 +0100

    Create 07_List_Comprehensions.py

---
+++ 07_List_Comprehensions.py
@@ -0,0 +1,6 @@
+# A sample list of numbers
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# Check for divisible numbers for each number in the list
+even_numbers = [number for number in a if number % 2 == 0]
+# Print the results
+print(even_numbers)
