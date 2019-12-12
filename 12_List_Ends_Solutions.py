b2baf188eeff8e141f7b650d01f7cef18e63f122
Łukasz Dziedzic <pymator@gmail.com>
2019-12-12 15:42:50 +0100

    Create 12_List_Ends_Solutions.py

---
+++ 12_List_Ends_Solutions.py
@@ -0,0 +1,11 @@
+# A sample list of numbers
+a = [5, 10, 15, 20, 25]
+
+
+def list_ends(numbers):
+    '''Return first and last element of the given list'''
+    return [numbers[0], numbers[-1]]
+
+
+# Print the results
+print(list_ends(a))
